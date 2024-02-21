05478f0b8904e14a76541a35e3d9f5f5576264ac
SqCodeJs <tomek.bilka@gmail.com>
2024-02-21 21:20:59 +0100

    adds Module WebInfo

---
+++ app/code/Task/WebInfo/Block/WebInfo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Task\WebInfo\Block;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\View\Element\Template\Context;
+
+class WebInfo extends \Magento\Framework\View\Element\Template
+{
+    protected $_urlInterface;
+    protected $scopeConfig;
+
+    public function __construct(
+        Context $context,
+        \Magento\Framework\UrlInterface $urlInterface,
+        ScopeConfigInterface $scopeConfig,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_urlInterface = $urlInterface;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function getPhoneNumber()
+    {
+        return $this->scopeConfig->getValue('general/store_information/phone',ScopeInterface::SCOPE_STORE);
+    }
+
+    public function getEmailUs()
+    {
+        return $this->scopeConfig->getValue('trans_email/ident_general/email',ScopeInterface::SCOPE_STORE);
+    }
+}
